6dbebb1b95645c332880263f319c0997f5ebbe76
mjkim0819 <108729047+mjkim0819@users.noreply.github.com>
2022-07-12 22:08:40 +0900

    Create [KMJ]Python: Division.py

---
+++ [01주차]/[Python: Division]/[KMJ]Python: Division.py
@@ -0,0 +1,4 @@
+a = int(input()) #a에 int형태의 첫번째 입력값을 받는다
+b = int(input())  #b에 int형태의 두번째 입력값을 받는다
+print(int(a/b))  #a/b를 int형태로 표현한다. 소숫점 아래 수 삭제
+print(float(a/b)) #a/b를 float형태로 표현한다. 소숫점 아래 수 보존
