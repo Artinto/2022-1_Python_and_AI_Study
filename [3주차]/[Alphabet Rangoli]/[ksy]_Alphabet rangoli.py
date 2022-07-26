9a1acef31243d47eecf933013c27a8982f876b30
king OF teeth <77234878+atgs6591@users.noreply.github.com>
2022-07-26 17:51:13 +0900

    Update [ksy]_Alphabet rangoli.py

--- [3주차]/[Alphabet Rangoli]/[ksy]_Alphabet rangoli.py
+++ [3주차]/[Alphabet Rangoli]/[ksy]_Alphabet rangoli.py
@@ -2,7 +2,7 @@ def print_rangoli(n):
   alpha='abcdefghijklmnopqrstuvwxyz'
   import string
   design = string.ascii_lowercase   #알파벳 소문자 모아둠
-  for i in range(n-1,-n, -1):
+  for i in range(n-1,-n, -1):  #5를기준 4 3 2 1 0 -1 -2 -3 -4 
     string1 = '-'.join(design[n-1:abs(i):-1] + design[abs(i):n])
     print(string1.center(4 * n - 3, '-'))
 print_rangoli(10)
