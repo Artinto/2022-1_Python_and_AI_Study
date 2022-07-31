a0203763db33ddc21a20a3da6551d66e1c71ae11
tk010125 <108648605+Tk010125@users.noreply.github.com>
2022-07-31 15:44:22 +0900

    Update {HTK}mutations.py

--- [4주차]/[mutations]/{HTK}mutations.py
+++ [4주차]/[mutations]/{HTK}mutations.py
@@ -1,2 +1,10 @@
 def mutate_string(string, position, character):
     a=string[:position]+character+string[position+1:]#정해진 포지션 사이에 넣어야 하는 값을 넣을 수 있도록 :pos, pos+1:을 이용
+    return a
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
+    
