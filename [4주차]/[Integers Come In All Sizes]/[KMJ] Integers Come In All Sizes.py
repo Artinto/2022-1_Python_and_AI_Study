c0c6c62094a7ca39f264a796e08ab80a33017648
mjkim0819 <108729047+mjkim0819@users.noreply.github.com>
2022-08-03 19:32:30 +0900

    Create [KMJ] Integers Come In All Sizes.py

---
+++ [4주차]/[Integers Come In All Sizes]/[KMJ] Integers Come In All Sizes.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+print((a**b)+(c**d))
+
+# x의 y제곱을 표현하는 방법은 두가지이다. 간단하게는 x**y로 표현할 수 있고, 다른 방식은 저번주에 사용한 pow(x,y)가 있다.
+# print(pow(a,b)+pow(c,d))로 표현할 수도 있다.
