603dc27e29d12e94dc5f532795aebe2ee07d3211
tk010125 <108648605+Tk010125@users.noreply.github.com>
2022-08-22 23:20:45 +0900

    Create [htk]counting sort 2.py

---
+++ [7주차]/[counting sort 2]/[htk]counting sort 2.py
@@ -0,0 +1,31 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    return sorted(arr)#정렬함수를 이용해 정렬을 반복하도록 설정
+            
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
